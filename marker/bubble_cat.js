2365326629677f8113fdbb9316727136dd94b423
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 15:45:41 +0700

    Update bubble_cat.js

--- marker/bubble_cat.js
+++ marker/bubble_cat.js
@@ -10,36 +10,8 @@ var bubble_cat = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    215.5,
-                    -77.75
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "id": "02",
-                "description": "."
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    150.25,
-                    -86.25
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "id": "03",
-                "description": "."
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    47.25,
-                    -189
+                    223.75,
+                    -104
                 ]
             }
         }
